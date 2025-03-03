c031c3b2a17cb5464f8af637a9b0238263b90e05
Joy Mukerjee <mukerjeejoy@protonmail.com>
2025-03-04 00:52:03 +0530

    Updated CPQ for PyPI release and fixed SSL issues

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='causal-priority-queue',
+    name='causal-priority-queue',  # Package name on PyPI
     version='1.0.0',
     packages=find_packages(),
     install_requires=[],  # No external dependencies
     author='Joy Mukerjee',
-    author_email='mukerjeejoy@protonmail.com.com',
+    author_email='mukerjeejoy@protonmail.com',
     description='A dynamically adjusting priority queue with causal influences.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/mukerjeejoy-github/causal-priority-queue',
+    url='https://github.com/mukerjeejoy-github/causal-priority-queue',  # Link to GitHub repo
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
